eb5ad4a6793b171bf8134f69546ff5e322f7bba1
Carlos Quiñones <34200427+carlos-quinones-diaz@users.noreply.github.com>
2021-06-21 18:39:42 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("--Suma de digitos--");
+
+            funciones digitos = new funciones();
+
+            Console.WriteLine(digitos.difitSum(12345));
+
+            Console.WriteLine("");
+
+
+
+            Console.WriteLine("--Palíndromos--");
+
+            funciones palindromo = new funciones();
+
+            Console.WriteLine(palindromo.ispalindrome("salas2"));
+
+            Console.WriteLine("");
+
+
+            Console.WriteLine("--Producto de elementos adyacentes--");
+
+            funciones adyacentes = new funciones();
+
+            Console.WriteLine(adyacentes.maxAdjacentProd(new int[] { 3,6,-2,-5,7,3 }));
+
+
+
+        }
+    }
+}
